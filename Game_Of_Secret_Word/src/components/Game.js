79b6fe34813745a193cbbf4ee2f322742ba9a0a2
PettersonWilliam <petterson309@gmail.com>
2023-08-07 08:43:32 -0300

    ft(secret-word-game): implementing new words in the game

--- Game_Of_Secret_Word/src/components/Game.js
+++ Game_Of_Secret_Word/src/components/Game.js
@@ -18,44 +18,42 @@ const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters
         lettersInputRef.current.focus();// foco na referencia apos o fim do onSubmit
     };
 
-  return (
-    <div className='game'>
-        <p className="points">
-            <span>Pontuação: {score}</span>
-        </p>
-        <h1>Advinhe a palavra</h1>
-        <h3 className='tip'>
-            Dica sobre a palavra:
-            <span>{pickedCategory}</span>
-        </h3>
-        <p>Você tem {guesses} tentativa(s).</p>
-        <div className="wordContainer">
-        {letters.map((letter, i) =>
-            guessedLetters.includes(letter) ? (
-                <span key={i} className='letter'>
-                    {letter}
-                </span>
-            ) : (
-            <span key={i} className='blankSquare'></span>
-            )
-        )}
+    return (
+        <div className='game'>
+            <p className="points">
+                <span>Pontuação: {score}</span>
+            </p>
+            <h1>Advinhe a palavra</h1>
+            <h3 className='tip'>
+                Dica sobre a palavra:
+                <span>{pickedCategory}</span>
+            </h3>
+            <p>Você tem {guesses} tentativa(s).</p>
+            <div className="wordContainer">
+                {letters.map((letter, i) => guessedLetters.includes(letter) ? (
+                    <span key={i} className='letter'>
+                        {letter}
+                    </span>
+                ) : (
+                    <span key={i} className='blankSquare'></span>
+                ))}
+            </div>
+            <div className="letterContainer">
+                <p>Tente Adivinhar a letra:</p>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name='letter' maxLength= '1' required onChange={(e) => setLetter(e.target.value)} value={letter} ref={lettersInputRef}//ref = seta referencia
+                    /> {/*alterando a letra com base no evento*/}
+                    <button>Jogar</button> {/* colocar uma função aqui */}
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras já ultilizadas: </p>
+                {wrongLetters.map((letter, i) => (
+                <span key={i}>{letter}, </span>
+                ))}
+            </div>
         </div>
-        <div className="letterContainer">
-            <p>Tente Adivinhar a letra:</p>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name='letter' maxLength= '1' required onChange={(e) => setLetter(e.target.value)} value={letter} ref={lettersInputRef}//ref = seta referencia
-                /> {/*alterando a letra com base no evento*/}
-                <button>Jogar</button> {/* colocar uma função aqui */}
-            </form>
-        </div>
-        <div className="wrongLettersContainer">
-            <p>Letras já ultilizadas: </p>
-            {wrongLetters.map((letter, i) => (
-            <span key={i}>{letter}, </span>
-            ))}
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Game;
